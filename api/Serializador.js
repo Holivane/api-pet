7263ccaad90b9eedadf3e6b441699ebafa5d18f2
Holivane <holivane@gmail.com>
2021-12-22 12:46:54 -0300

    Criação de campos extras para idFornecedor

--- api/Serializador.js
+++ api/Serializador.js
@@ -40,19 +40,30 @@ class Serializador {
     }
 }
 class SerialiazadorFornecedor extends Serializador {
-    constructor(contentType) {
+    constructor(contentType, camposExtras) {
         super()
         this.contentType = contentType
         this.camposPublicos = [
             'id', 
             'empresa', 
             'categoria'
-        ]
+        ].concat(camposExtras || [])
+    }
+}
+class SerializadorErro extends Serializador {
+    constructor (contentType, camposExtras) {
+        super()
+        this.contentType = contentType
+        this.camposPublicos = [
+            'id',
+            'mensagem'
+        ].concat(camposExtras || [])
     }
 }
 
 module.exports = {
     Serializador: Serializador, 
     SerialiazadorFornecedor: SerialiazadorFornecedor,
+    SerializadorErro: SerializadorErro,
     formatosAceitos: ['application/json']
 }
--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ const CampoInvalido = require('./erros/CampoInvalido')
 const DadosNaoFornecidos = require('./erros/DadosNaoFornecidos')
 const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
 const formatosAceitos = require('./Serializador').formatosAceitos
+const SerializadorErro = require('./Serializador').SerializadorErro
 
 app.use(bodyParser.json())
 
@@ -45,9 +46,12 @@ app.use((erro, req, res, proximo) => {
         status = 406
     }
 
+    const serializador = new SerializadorErro (
+        res.getHeader('Content-Type')        
+    )
     res.status(status)
     res.send(
-        JSON.stringify({
+        serializador.serializar({
             mensagem: erro.message,
             id: erro.idErro
         })
