f3e4a5f347df024bb562d6dab9f05b83ba3b116c
Holivane <holivane@gmail.com>
2022-01-28 15:04:37 -0300

    Criação de rota para atualizar

--- api/rotas/fornecedores/produtos/Produto.js
+++ api/rotas/fornecedores/produtos/Produto.js
@@ -1,4 +1,3 @@
-const res = require('express/lib/response')
 const Tabela = require('./TabelaProduto')
 
 class Produto {
@@ -50,6 +49,34 @@ class Produto {
         this.dataCriacao = produto.dataCriacao
         this.dataAtualizacao = produto.dataAtualizacao
         this.versao = produto.versao
+    }
+
+    atualizar() {
+        const dadosParaAtualizar = {}
+
+        if (typeof this.titulo === 'string' && this.titulo.length > 0) {
+            dadosParaAtualizar.titulo = this.titulo
+        }
+        
+        if (typeof this.preco === 'number' && this.preco > 0) {
+            dadosParaAtualizar.preco = this.preco
+        }
+        
+        if (typeof this.estoque === 'number') {
+            dadosParaAtualizar.estoque = this.estoque
+        }
+
+        if (Object.keys(dadosParaAtualizar).length === 0) {
+            throw new Error ('Não foram fornecidos dados para atualizar')
+        }
+
+        return Tabela.atualizar(
+            {
+                id: this.id,
+                fornecedor: this.fornecedor
+            },
+            dadosParaAtualizar
+        )
     }
 }
 
